ebd27b0d9e9ba29f3eda6ed5939e4fc89089eab1
Sibiraj <Sibiraj@DMN-SIBI>
2017-03-22 15:16:24 +0400

    Addedd FileFlush App

---
+++ FileFlush/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+
+namespace FileFlush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+                Utilities.WriteLog("Process Started:" + String.Format("{0:f}", DateTime.Now));
+
+                //Get Folder Path to be deleted
+                string path = System.Configuration.ConfigurationSettings.AppSettings["Path"];
+
+                //All Folders
+                foreach (string directory in Directory.GetDirectories(path))
+                {
+                    //All Files
+                    foreach (string file in Directory.GetFiles(directory))
+                    {
+                        Utilities.WriteLog("deleting " + file);
+                        File.Delete(file);
+                    }
+                    //Sub Folders
+                    foreach (string Folders in Directory.GetDirectories(directory))
+                    {
+                        Utilities.WriteLog("deleting " + Folders);
+                        DeleteDirectory(Folders);
+                    }
+
+                }
+
+                Utilities.WriteLog("Process Finished:" + String.Format("{0:f}", DateTime.Now));
+                Utilities.WriteLog("__________________________________");
+            }
+            catch (Exception ex)
+            {
+                Utilities.WriteLog("Error");
+                Utilities.WriteLog(ex.Message.ToString());
+                Utilities.WriteLog("Process Finished:" + String.Format("{0:f}", DateTime.Now));
+                Utilities.WriteLog("__________________________________");
+            }
+        }
+        private static void DeleteDirectory(string path)
+        {
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    //Delete all files 
+                    foreach (string file in Directory.GetFiles(path))
+                    {
+                        File.Delete(file);
+                    }
+                    //Delete all Sub Directories
+                    foreach (string directory in Directory.GetDirectories(path))
+                    {
+                        DeleteDirectory(directory);
+                    }
+                    //Delete a Folder
+                    Directory.Delete(path, true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Utilities.WriteLog("Delete failed.");
+                Utilities.WriteLog(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ FileFlush/Utilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileFlush
+{
+    public class Utilities
+    {
+        public static FileStream fileStream;
+        public static StreamWriter streamWriter;
+        public static void OpenFile()
+        {
+            string strPath = null;
+            strPath = Environment.CurrentDirectory + "\\Log.log";
+            if (System.IO.File.Exists(strPath))
+            {
+                fileStream = new FileStream(strPath, FileMode.Append, FileAccess.Write);
+            }
+            else
+            {
+                fileStream = new FileStream(strPath, FileMode.Create, FileAccess.Write);
+            }
+            streamWriter = new StreamWriter(fileStream);
+        }
+        public static void WriteLog(string strComments)
+        {
+            OpenFile();
+            streamWriter.WriteLine(strComments);
+            CloseFile();
+        }
+        public static void CloseFile()
+        {
+            streamWriter.Close();
+            fileStream.Close();
+        }
+    }
+}
