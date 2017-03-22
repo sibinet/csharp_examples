f801519381c25cfcddd6a6b5dc8c0019d42eeb05
Sibiraj <Sibiraj@DMN-SIBI>
2017-03-22 15:45:03 +0400

    Added Filename Sanitize

---
+++ SanitizeFileName/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanitizeFileName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string checkfilename = "* somefile.txt";
+            string clearfilename = FileUtility.SanitizeFileName(checkfilename);
+            //
+            //output
+            //_somefile.txt
+        }
+    }
+}
